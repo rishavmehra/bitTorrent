1c6d45fe179ec6dab4e4b1b278cdf5998de2610f
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-12 16:16:36 +0530

    read and extract the information from the .torrent file

--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -2,6 +2,7 @@ package torrentfile
 
 import (
 	"bytes"
+	"crypto/sha1"
 	"fmt"
 	"os"
 
@@ -17,7 +18,7 @@ type bencodeInfo struct {
 	Pieces      string `bencode:"pieces"`
 	PieceLength int    `bencode:"piece length"`
 	Name        string `bencode:"name"`
-	Length      string `bencode:"length"`
+	Length      int    `bencode:"length"`
 }
 
 type TorrentFile struct {
@@ -29,26 +30,63 @@ type TorrentFile struct {
 	Name        string
 }
 
-func Open(path string) (*TorrentFile, error) {
+func Open(path string) (TorrentFile, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return &TorrentFile{}, err
+		return TorrentFile{}, err
 	}
 	defer file.Close()
 
 	bto := bencodeTorrent{}
-	err = bencode.Unmarshal(file, &bto)
+	err = bencode.Unmarshal(file, &bto) // convert into Go data structure
 	if err != nil {
-		return &TorrentFile{}, err
+		return TorrentFile{}, err
 	}
 	fmt.Println(bto)
 	return bto.toTorrentFile()
 }
 
 func (bto *bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return TorrentFile{}, fmt.Errorf("Error in PieceHashes: ", err)
+	}
+	t := TorrentFile{
+		Announce:    bto.Announce,
+		InfoHash:    infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Lenght:      bto.Info.Length,
+		Name:        bto.Info.Name,
+	}
+	return t, nil
+}
 
+func (i *bencodeInfo) splitPieceHashes() ([][20]byte, error) {
+	hasLen := 20
+	buf := []byte(i.Pieces)
+	if len(buf)%hasLen != 0 {
+		err := fmt.Errorf("received malformed pieces of length %d", len(buf))
+		return nil, err
+	}
+	numHashes := len(buf) / hasLen
+	hashes := make([][20]byte, numHashes)
+	for i := 0; i < numHashes; i++ {
+		copy(hashes[i][:], buf[i*hasLen:(i+1)*hasLen])
+	}
+	return hashes, nil
 }
 
 func (i *bencodeInfo) hash() ([20]byte, error) {
 	var buf bytes.Buffer
+	err := bencode.Marshal(&buf, i) //
+	if err != nil {
+		return [20]byte{}, err
+	}
+	h := sha1.Sum(buf.Bytes())
+	return h, nil
 }
