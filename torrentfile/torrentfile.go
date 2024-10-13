ddc3afe0de4e58e1c2551afb45f4865bae7a734f
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-13 13:29:24 +0530

    ref: Structure the code

--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -9,27 +9,6 @@ import (
 	"github.com/jackpal/bencode-go"
 )
 
-type bencodeTorrent struct {
-	Announce string      `bencode:"announce"`
-	Info     bencodeInfo `bencode:"info"`
-}
-
-type bencodeInfo struct {
-	Pieces      string `bencode:"pieces"`
-	PieceLength int    `bencode:"piece length"`
-	Name        string `bencode:"name"`
-	Length      int    `bencode:"length"`
-}
-
-type TorrentFile struct {
-	Announce    string
-	InfoHash    [20]byte
-	PieceHashes [][20]byte
-	PieceLength int
-	Lenght      int
-	Name        string
-}
-
 func Open(path string) (TorrentFile, error) {
 	file, err := os.Open(path)
 	if err != nil {
