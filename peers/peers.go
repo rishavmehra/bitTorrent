5bd774ba71dabf2e31c2b26dcdb1fd3aea1b1732
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-13 13:28:39 +0530

    response from peers, Unmarshal the ip,port form the buffer

---
+++ peers/peers.go
@@ -0,0 +1,32 @@
+package peers
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+/*
+check the Response Formate, We are building this now
+https://wiki.theory.org/BitTorrent_Tracker_Protocol
+*/
+type Peer struct {
+	IP   net.IP
+	Port uint16
+}
+
+func Unmarshall(peersBin []byte) ([]Peer, error) {
+	const peerSize = 6
+	numPeers := len(peersBin) / 6
+	if len(peersBin)%peerSize != 0 {
+		err := fmt.Errorf("received wrong peers")
+		return nil, err
+	}
+	peers := make([]Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		offset := i * peerSize
+		peers[i].IP = net.IP(peersBin[offset : offset+4])
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
+	}
+	return peers, nil
+}
