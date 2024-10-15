262824c897de8f190ffb2bb3dbde983a455c3b6a
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-15 22:41:22 +0530

    added torrefile struct in one place

--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -9,6 +9,27 @@ import (
 	"github.com/jackpal/bencode-go"
 )
 
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Name        string `bencode:"name"`
+	Length      int    `bencode:"length"`
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Lenght      int
+	Name        string
+}
+
 // https://www.bittorrent.org/index.html
 func Open(path string) (TorrentFile, error) {
 	file, err := os.Open(path)
