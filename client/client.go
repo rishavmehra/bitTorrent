732ff93fdc50c351b5dc2d869130f78d8de91a6f
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-15 19:02:10 +0530

    helper function for peer to peer communication

--- client/client.go
+++ client/client.go
@@ -1,17 +1,126 @@
 package client
 
 import (
+	"bytes"
+	"fmt"
 	"net"
 	"time"
 
+	"github.com/rishavmehra/bitTorrent/bitfield"
+	"github.com/rishavmehra/bitTorrent/handshake"
+	"github.com/rishavmehra/bitTorrent/message"
 	"github.com/rishavmehra/bitTorrent/peers"
 )
 
-func New(peer peers.Peer, peerID, infoHash [20]byte) {
+type Client struct {
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.Bitfield
+	peer     peers.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
+
+func completeHandshake(conn net.Conn, infohash, peerId [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	req := handshake.New(infohash, peerId)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if !bytes.Equal(res.InfoHash[:], infohash[:]) {
+		return nil, fmt.Errorf("expected  infohash %x but got %x", res.InfoHash, infohash)
+	}
+	return res, nil
+}
+
+func recvBitfield(conn net.Conn) (bitfield.Bitfield, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if msg == nil {
+		err := fmt.Errorf("Expected bitfield but got %s", msg)
+		return nil, err
+	}
+
+	if msg.ID != message.MsgBitfield {
+		return nil, fmt.Errorf("execpted bitfield %d but got %d", message.MsgBitfield, msg.ID)
+	}
+	return msg.Payload, err
+}
+
+func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 	conn, err := net.DialTimeout("tcp", peer.String(), 5*time.Second)
 	if err != nil {
 		return nil, err
 	}
 
-	// need handshake
+	_, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	// bitfield message may only be sent immediately after the handshaking sequence
+	bf, err := recvBitfield(conn)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, err
+}
+
+func (c *Client) Read() (*message.Message, error) {
+	msg, err := message.Read(c.Conn)
+	return msg, err
+}
+
+func (c *Client) SendRequest(index, begin, length int) error {
+	req := message.FormatRequest(index, begin, length)
+	_, err := c.Conn.Write(req.Serialize())
+	return err
+}
+
+func (c *Client) SendInterested() error {
+	msg := message.Message{ID: message.MsgInterested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendNotInterested() error {
+	msg := message.Message{ID: message.MsgNotInterested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendUnchoke() error {
+	msg := message.Message{ID: message.MsgUnchoke}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendHave(index int) error {
+	msg := message.FormatHave(index)
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
 }
